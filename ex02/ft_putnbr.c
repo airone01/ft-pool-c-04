332b3b027fac874d324ef6e2effbc07e2cd3c495
elagouch <elagouch@42>
2024-09-18 12:57:34 +0200

    feat(ex02): implement

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:42:17 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/18 12:42:18 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+// #include <limits.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+		return ;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+		return ;
+	}
+	ft_putchar(nb + '0');
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr(42);
+// 	ft_putchar('\n');
+// 	ft_putnbr(-42);
+// 	ft_putchar('\n');
+// 	ft_putnbr(-2147483648);
+// 	ft_putchar('\n');
+// 	ft_putnbr(2147483647);
+// 	ft_putchar('\n');
+// }
